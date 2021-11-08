7b612eb54197309b335a5eded6736a86708db19d
bhughes20 <74008833+bhughes20@users.noreply.github.com>
2021-11-08 10:19:00 +0000

    Test with fail()

--- src/main/java/com/example/reactbackend/repository/PersonRepository.java
+++ src/main/java/com/example/reactbackend/repository/PersonRepository.java
@@ -3,5 +3,9 @@ package com.example.reactbackend.repository;
 import com.example.reactbackend.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    List<Person> findByFirstNameAndLastName();
 }
